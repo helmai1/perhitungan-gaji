35ed659bd11520f50b5f56270491a369f24ea0c8
berlianafd <berlianafd@gmail.com>
2020-08-16 18:00:52 +0700

    register react

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Http\Request;
 */
 Route::post('login', 'UserController@login');
 
+Route::post('register', 'UserController@register');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
